ebbcc26dbba37c174384a8cb92cbd7702fe65a71
Sougata1233 <sougata.roy9203@gmail.com>
2018-11-29 15:33:43 +0530

    pandas, matplotlib added

--- numpyprac.py
+++ numpyprac.py
@@ -13,6 +13,10 @@
 # much more effectively.
 
 
+# the most frequently performed operations in Numpy, such as, working with N-dimensional array, 
+# Indexing and slicing of arrays, Indexing using integer arrays, transposing an array, universal functions, 
+# data processing using arrays, frequently used statistical methods, etc
+
 
 
 import numpy as np
@@ -146,3 +150,107 @@ print("value of mod "+ str(np.modf(x,y)))
 
 #Data processing using array
 print("Data processing using array")
+mtrx = np.arange(-5,5,1)
+a, b = np.meshgrid(mtrx, mtrx)
+print("value of x: {}".format(x))
+print("value of y: {}".format(y))
+
+x1 = np.array([1,2,3,4,5])
+y1 = np.array([6,7,8,9,10])
+cond = [True, False, True, True, False]
+#If you want to take a value from x1 whenever the corresponding value in cond is true,otherwise take value from y.
+print("If you want to take a value from x1 whenever the corresponding value in cond is true,otherwise take value from y.")
+z1 = [(x,y,z) for x,y,z in zip(x1, y1, cond)]
+print(z1)
+print(np.where(cond, x1, y1))
+ra = np.random.randn(5,5)
+print("original value")
+print(ra)
+print("replacing -ve values wit -1 and positives with 1")
+print(np.where(ra>0, 1, -1))
+print("replacing only positive values with 1")
+print(np.where(ra>0,1,ra))
+print("replacing only negative values with -1")
+print(np.where(ra>0,ra,-1))
+
+#Statistical Methods
+print("Statistical Methods")
+thie =  np.random.randn(5,5)
+print("calculating mean value")
+print(thie.mean())
+print("calculating mean value with numpy module")
+print(np.mean(thie))
+print("calculating sum")
+print(thie.sum())
+jp = np.arange(12).reshape(4,3)
+print("Arrays are: {}".format(jp))
+#axis =0, gives you sum of the columns. axis =1 , gives sum of rows.
+# remember this zero is for columns and one is for rows.
+print("axis =0, gives you sum of the columns. axis =1 , gives sum of rows.")
+print("remember this zero is for columns and one is for rows.")
+print("Sum of rows are: {}".format(np.sum(jp, axis=0)))
+print("sum of rows: {}".format(jp.sum(1)))
+print("cumulative sum of columns: {}".format(jp.cumsum(0)))
+print("cumulative sum of rows: {}".format(jp.cumsum(1)))
+
+xp = np.random.randn(100)
+print("sum of all positive values: {}".format((xp>0).sum()))
+print("sum of all negative values: {}".format((xp<0).sum()))
+tandf =np.array([True,False,True,False,True,False])
+print("checks if any of the values are true: {}".format(tandf.any()))
+print("returns false even a single value is true: {}".format(tandf.all()))
+print("standard daviation: {}".format(xp.std()))
+print("variance: {}".format(xp.var()))
+print("indices of max elements: {}".format(xp.argmax()))
+print("indices of min elements: {}".format(xp.argmin()))
+
+#Sorting
+print("Sorting")
+lp = np.random.randn(8)
+print("original: {}".format(lp))
+lp.sort()
+print("after sorting: {}".format(lp))
+personals = np.array(['Manu', 'Jeevan', 'Prakash', 'Manu', 'Prakash', 'Jeevan', 'Prakash'])
+print("returns unique elements: {}".format(np.unique(personals)))
+print("set() also return unique elements: {}".format(set(personals)))
+print("in1d() checks for a value and returns boolean: {}".format(np.in1d(personals, ['Manu'])))
+
+#Linear Algebra
+print("Linear Algebra")
+cp = np.array([[1,2,3],[4,5,6]])
+dp = np.array([[7,8],[9,10],[11,12]])
+print("cp array: {}".format(cp))
+print("dp array: {}".format(dp))
+print("element wise multiplication (dot product): {}".format(cp.dot(dp)))
+print("dot product: {}".format(np.dot(cp, np.ones(3))))
+# numpy.linalg has standard matrix operations like determinants and inverse.
+from numpy.linalg import inv, qr
+new_mat = cp.T.dot(cp) #mulplying cp inverse with cp which is element wise multiplication
+print("mulplying cp inverse with cp which is element wise multiplication: {}".format(new_mat))
+sp = np.random.randn(5,5)
+print("inverse: {}".format(inv(sp)))
+q,r = qr(sp)
+print("********")
+print(q)
+print("********")
+print(r)
+print("*******")
+print("return the diagonal elements of a square matrix in 1D: {}".format(np.diag(sp, k=0)))
+print("*******")
+print("compute the sum of diagonal elements: {}".format(np.trace(sp)))
+print("*******")
+print("compute matrix determinant: {}".format(np.linalg.det(sp)))
+print("*******")
+print("compute the eigenvalue: {}".format(np.linalg.eig(sp)))
+print("*******")
+print("compute the pseudo inverse: {}".format(np.linalg.pinv(sp)))
+print("singular value decomposition: {}".format(np.linalg.svd(sp)))
+print("*******")
+a = np.array([[3,1], [1,2]])
+b = np.array([9,8])
+print("Solve the linear system Ax = b for x, where A is a square matrix: {}".format(np.linalg.solve(a,b)))
+print("*******")
+x = np.array([0, 1, 2, 3])
+y = np.array([-1, 0.2, 0.9, 2.1])
+A = np.vstack([x, np.ones(len(x))]).T
+print("compute least-square solution to y=Xb: {}".format(np.linalg.lstsq(A, y)[0]))
